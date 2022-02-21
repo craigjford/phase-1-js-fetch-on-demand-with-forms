32cb6cdc66e34123c5075e9e2701ada2ce6bad32
Craig Ford <craig_ford@icloud.com>
2022-02-21 14:16:27 -0500

    commit tree 50a3aa29a526ebec330b95e9790e50d03384e1e4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        console.log(input.value);
+        const boolTrue = editID(input.value);
+
+        if (boolTrue) {
+            fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())
+            .then(data => {
+                const title = document.querySelector('section#movieDetails h4');
+                const summary = document.querySelector('section#movieDetails p');
+
+                title.innerText = data.title;
+                summary.innerText = data.summary;
+            });
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function editID(id) {
+    if (parseInt(id) < 4  && parseInt(id) > 0) {
+        return true;
+    } else {
+        alert('Invalid Id - Please try again')
+        const idIn= document.querySelector('input#searchByID');
+        idIn.value = "";
+        return false;
+    }
+}
